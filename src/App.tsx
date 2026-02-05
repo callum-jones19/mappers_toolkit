8137db9557fde6fd809c77c7647ce857dbf34199
Callum Jones <callum@bazzite.modem>
2026-02-05 14:42:50 +1100

    Add helper toastt

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { Map, Marker } from '@vis.gl/react-maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import { useState } from 'react';
-import { MapPin } from 'react-feather';
+import { useMemo, useState } from 'react';
+import { Info, MapPin } from 'react-feather';
 
 interface Point {
 	latitude: number;
 	longitude: number;
 }
 
-interface Line {
-	points: Point[];
-}
+// interface Line {
+// 	points: Point[];
+// }
 
-type ActiveAction = "Default" | "PlacePoint" | "PlaceLine" | "PlacePolygon";
+type ActiveAction = "Default" | "PlacePoint" | "PlaceLine" | "PlacePolygon" | "DeletePoint";
 
 function App() {
 
@@ -24,7 +24,25 @@ function App() {
   // App state
   const [activeAction, setActiveAction] = useState<ActiveAction>("Default");
   const [points, setPoints] = useState<Point[]>([]);
-  const [lines, setLines] = useState<Line[]>([]);
+  // const [lines, setLines] = useState<Line[]>([]);
+  
+  const displayToast = useMemo(() => {
+	if (activeAction === 'PlacePoint') {
+		return true;
+	} else {
+		return false;
+	}
+  }, [activeAction]);
+
+  const toastText = useMemo(() => {
+	if (activeAction === 'Default') {
+		return "";
+	} else if (activeAction === "PlacePoint") {
+		return "Left click the map to place a point at the cursor's location"
+	}
+  }, [activeAction]);
+
+
 
   return (
     <>
@@ -33,9 +51,9 @@ function App() {
 			<div id='tools-section' className='w-full h-full flex flex-col p-2 gap-2 overflow-auto'>
 				<input type='search' className='w-full bg-neutral-300 p-2 rounded-sm sticky' placeholder='Search tool...' />
 				<details open className='w-full'>
-					<summary className='font-bold'>Inputs</summary>
+					<summary className='font-bold'>Draw</summary>
 					<button
-						className={`mt-2 w-full p-1 ${activeAction === 'PlacePoint' ? "bg-neutral-400" : "bg-neutral-100 hover:bg-neutral-200"} rounded-t-sm`}
+						className={`mt-2 w-full p-1 ${activeAction === 'PlacePoint' ? "bg-neutral-400 animate-pulse" : "bg-neutral-100 hover:bg-neutral-200"} rounded-t-sm`}
 						onClick={() => {
 							if (activeAction === 'PlacePoint') {
 								setActiveAction('Default');
@@ -46,43 +64,79 @@ function App() {
 					>
 						Add point
 					</button>
-					<button className='mt-1 w-full p-1 hover:bg-neutral-200 bg-neutral-100'>Add polygon</button>
-					<button className='mt-1 w-full p-1 hover:bg-neutral-200 bg-neutral-100 rounded-b-sm'>Add line</button>
-					<button className='mt-1 w-full p-1 hover:bg-neutral-200 bg-neutral-100 rounded-b-sm'>Import GeoJSON</button>
+					<button disabled className='mt-1 disabled:bg-neutral-500 disabled:text-neutral-400 w-full p-1 hover:bg-neutral-200 bg-neutral-100'>Add polygon</button>
+					<button disabled className='mt-1 disabled:bg-neutral-500 disabled:text-neutral-400 w-full p-1 hover:bg-neutral-200 bg-neutral-100'>Add line</button>
+					<button disabled className='mt-1 disabled:bg-neutral-500 disabled:text-neutral-400 w-full p-1 hover:bg-neutral-200 bg-neutral-100 rounded-b-sm'>Import GeoJSON</button>
+				</details>
+				<details open className='w-full'>
+					<summary className='font-bold'>Erase</summary>
+					<button
+						className={`mt-2 w-full p-1 ${activeAction === 'DeletePoint' ? "bg-neutral-400 animate-pulse" : "bg-neutral-100 hover:bg-neutral-200"} rounded-t-sm`}
+						onClick={() => {
+							if (activeAction !== 'DeletePoint') {
+								setActiveAction("DeletePoint");
+							} else {
+								setActiveAction("Default");
+							}
+						}}
+					>
+						Erase point
+					</button>
+					<button
+						className={`mt-1 w-full p-1 bg-neutral-100 hover:bg-neutral-200 rounded-b-sm`}
+						onClick={() => {
+							setPoints([]);
+						}}
+					>
+						Erase all points
+					</button>
 				</details>
 			</div>
 		</div>
-	    <Map
-		  style={{ height: '100%', flexGrow: 1 }}
-		  latitude={lat}
-		  longitude={lng}
-		  zoom={zoom}
-		  pitch={0}
-		  bearing={0}
-		  onDrag={e => {
-			  setLat(e.viewState.latitude);
-			  setLng(e.viewState.longitude);
-		  }}
-		  onZoom={e => {
-			  setLng(e.viewState.longitude);
-			  setLat(e.viewState.latitude);
-			  setZoom(e.viewState.zoom);
-		  }}
-		  mapStyle="https://tiles.versatiles.org/assets/styles/shadow/style.json"
-		  onClick={e => {
-			  if (activeAction === 'PlacePoint') {
-				  const newPoint: Point = { latitude: e.lngLat.lat, longitude: e.lngLat.lng };
-				  setPoints([...points, newPoint]);
-				  setActiveAction('Default');
-			  }
-		  }}
-	    >
-			{points.map(point => (
-				<Marker longitude={point.longitude} latitude={point.latitude}>
-					<MapPin className='text-white fill-neutral-500' />
-				</Marker>
-			))}
-		</Map>
+		<div className='w-full h-full relative'>
+			{displayToast && <div
+				id='toast'
+				className='absolute z-20 h-14 p-4 min-w-30 max-w-3/4 bottom-10 left-10 bg-white shadow-md rounded-sm'
+			>
+				<div className='flex flex-row gap-3 justify-between h-full w-full items-center'>
+					<Info />
+					<p>{toastText}</p>
+				</div>
+			</div>}
+			<Map
+			  style={{ height: '100%', flexGrow: 1 }}
+			  latitude={lat}
+			  longitude={lng}
+			  zoom={zoom}
+			  pitch={0}
+			  bearing={0}
+			  onDrag={e => {
+				  setLat(e.viewState.latitude);
+				  setLng(e.viewState.longitude);
+			  }}
+			  onZoom={e => {
+				  setLng(e.viewState.longitude);
+				  setLat(e.viewState.latitude);
+				  setZoom(e.viewState.zoom);
+			  }}
+			  mapStyle="https://tiles.versatiles.org/assets/styles/shadow/style.json"
+			  onClick={e => {
+				  if (activeAction === 'PlacePoint') {
+					  const newPoint: Point = { latitude: e.lngLat.lat, longitude: e.lngLat.lng };
+					  setPoints([...points, newPoint]);
+					  setActiveAction('Default');
+				  } else if (activeAction === 'DeletePoint') {
+					  console.log(e.type);
+				  }
+			  }}
+			>
+				{points.map(point => (
+					<Marker longitude={point.longitude} latitude={point.latitude}>
+						<MapPin className='text-white fill-neutral-500' />
+					</Marker>
+				))}
+			</Map>
+		</div>
 	  </div>
     </>
   )
