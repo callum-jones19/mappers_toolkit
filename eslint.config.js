63fdf9b35f87bda0231baa642ed68f74a3b0f6bc
Callum Jones <callum.jones+git@protonmail.com>
2026-02-16 14:55:15 +1100

    Enforce stricter eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import reactDom from "eslint-plugin-react-dom";
+import reactX from "eslint-plugin-react-x";
 
 export default defineConfig([
   globalIgnores(["dist"]),
@@ -11,9 +13,14 @@ export default defineConfig([
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+			tseslint.configs.strictTypeChecked,
+			tseslint.configs.strictTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+			reactX.configs["recommended-typescript"],
+      // Enable lint rules for React DOM
+      reactDom.configs.recommended,
+
     ],
     languageOptions: {
       ecmaVersion: 2020,
