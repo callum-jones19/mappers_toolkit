4a1a8d9fe9c90c1e263e966f082360194013e645
Callum Jones <callum.jones+git@protonmail.com>
2026-02-25 11:39:18 +1100

    Add active state to MappingButton

--- src/ui/MappingButton.tsx
+++ src/ui/MappingButton.tsx
@@ -12,7 +12,7 @@ export default function MappingButton({ isActive, children, ...buttonProps }: Ma
     <button
       className={`text-start rounded-sm flex flex-row justify-start items-center text-sm gap-2 w-full px-1 py-2 disabled:italic disabled:outline-none disabled:text-neutral-600 disabled:cursor-not-allowed ${
         isActive ? "bg-blue-500 text-white font-semibold" : "hover:outline-blue-500 hover:outline-2 hover:text-blue-500"
-      }`}
+      } active:bg-blue-500 active:text-white active:font-semibold`}
       {...buttonProps}
     >
       {children}
